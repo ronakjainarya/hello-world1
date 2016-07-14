13728e206bf5e2f4c18981e4edffd487d9ea2548
ronak.maru <ronak.maru@mcarbon.com>
2016-07-14 15:05:18 +0530

    commit

--- ABC2.java
+++ ABC2.java
@@ -1,7 +1,7 @@
 public class ABC1{
 	public static void main(String a[]){
 		System.out.println("Heloo");System.out.println("Heloo");
-		System.out.println("Heloo");
+		System.out.println("Helodo");
 		System.out.println("Heloo");
 		
 	}
